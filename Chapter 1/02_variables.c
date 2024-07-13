5dd98266998ed5b9930431b5556dbd528b9b021f
Sahil zagade <148172621+Samy-20@users.noreply.github.com>
2024-07-13 08:37:30 +0530

    Add files via upload

---
+++ Chapter 1/02_variables.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 4;
+    float b = 9.5;
+    char c = 'a';
+    int d = 35;
+    int e = 35 + 4;
+    printf("The value of a is %d \n", a);
+    printf("The value of b is %0.3f \n", b);
+    printf("The value of c is %c \n", c);
+    printf("Sum of a and d is %d \n", a - d);
+    printf("Sum of a and d is %d \n", e);
+    return 0;
+}
+
